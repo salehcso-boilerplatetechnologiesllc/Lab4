045ffd299be6711fa1b197aed37aa4a77de33440
Mohmed Saleh <salehcso@umich.edu>
2025-10-02 17:50:52 -0400

    Update Hello.java in new-feature branch

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         for(int i = 1; i <= 10; i++) {
-            System.out.println("Hello CIS 285..." + i);
+            System.out.println("This is from the new-feature branch... " + i);
+
         }
     }
 }
